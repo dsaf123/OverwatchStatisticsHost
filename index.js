161806d218cb909919c167c74c53eb3837f80963
Andrew <achcdp@mst.edu>
2018-11-15 17:15:42 +0000

    Updated for react

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ app.get('/cmpplayer', (req, res) => {
     });
   })
 });
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 app.set('port', (process.env.PORT || 4000))
 app.listen('port', () => {
   console.log("Server listening on 4000");
