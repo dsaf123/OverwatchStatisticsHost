211283f68aee4de7c80e1dbd1219c680425b996a
Andrew <achcdp@mst.edu>
2018-11-15 23:10:13 +0000

    Fixed issue

--- index.js
+++ index.js
@@ -86,12 +86,12 @@ app.get('/cmpplayer', (req, res) => {
 
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../webapp/build')))
+app.use(express.static(path.join(__dirname, './webapp/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../webapp/build/index.html'))
+  res.sendFile(path.join(__dirname + './webapp/build/index.html'))
 })
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log("Server listening on 4000");
 });
